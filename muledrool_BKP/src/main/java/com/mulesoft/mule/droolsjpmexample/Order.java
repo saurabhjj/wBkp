e9ed5e54759706bc17fa9c28c64a2864c1db7889
saurabhjj <saurabhjj@gmail.com>
2018-05-28 13:10:42 +0530

    Add files via upload

---
+++ muledrool_BKP/src/main/java/com/mulesoft/mule/droolsjpmexample/Order.java
@@ -0,0 +1,30 @@
+package com.mulesoft.mule.droolsjpmexample;
+
+public class Order {
+
+	private String merchant;
+	private double weight;
+	private String destination;
+	
+	public String getMerchant() {
+		return merchant;
+	}
+	public void setMerchant(String merchant) {
+		this.merchant = merchant;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	
+	
+	
+}
